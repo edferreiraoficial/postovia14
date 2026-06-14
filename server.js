4590eab12d0857aeebebd878ad6e2c179aefb4cf
Edson <edferreira.oficial@gmail.com>
2026-06-14 10:45:04 -0300

    Adiciona dotenv ao servidor minimo

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const upload = multer({ storage: multer.memoryStorage() });
+dotenv.config();
 
 app.get('/', (req, res) => {
-  res.send('Servidor mínimo funcionando');
+  res.send('Servidor mínimo funcionando com multer');
 });
 
 app.get('/health', (req, res) => {
