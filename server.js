42215f396ba5a5dc9406d22fa9ae0e7fed19683a
Edson <edferreira.oficial@gmail.com>
2026-06-14 16:16:43 -0300

    Testa gerarAuxiliarBanco

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-import express from 'express';
-import { importarPdfsBanco } from './backend/importarPdfsBanco.js';
+import { gerarPlanilhaAuxiliarDoBanco } from './backend/gerarAuxiliarBanco.js';
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-console.log('importarPdfsBanco carregado:', !!importarPdfsBanco);
-
-app.get('/', (req, res) => {
-  res.send('Importar PDFs carregado');
-});
-
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' });
-});
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+console.log(
+  'gerarPlanilhaAuxiliarDoBanco:',
+  !!gerarPlanilhaAuxiliarDoBanco
+);
