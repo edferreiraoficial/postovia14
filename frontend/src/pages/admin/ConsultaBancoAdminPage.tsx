7866a09b785b56c6ef81af0e4e5b66c89e90c988
Edson <edferreira.oficial@gmail.com>
2026-09-01 22:28:00 -0300

    Ajusta tamanho descrição extrato bancário

--- frontend/src/pages/admin/ConsultaBancoAdminPage.tsx
+++ frontend/src/pages/admin/ConsultaBancoAdminPage.tsx
@@ -898,7 +898,7 @@ export default function ConsultaBancoAdminPage() {
                 <tr>
                   <th style={{ ...estilosColunas.esquerda, width: '11ch' }}>Data</th>
                   {podeNumeroLancamento && <th style={{ ...estilosColunas.direita, width: '10ch' }}>Nº Lanc</th>}
-                  <th style={{ ...estilosColunas.esquerda, width: '61ch' }}>Descrição</th>
+                  <th style={{ ...estilosColunas.esquerda, width: '122ch' }}>Descrição</th>
                   <th style={{ ...estilosColunas.direita, width: '15ch' }}>Natureza</th>
                   <th style={{ ...estilosColunas.direita, width: '14ch' }}>Valor</th>
                   <th style={{ ...estilosColunas.direita, width: '14ch' }}>Saldo</th>
@@ -912,7 +912,7 @@ export default function ConsultaBancoAdminPage() {
                       <td>{campoEdicao('data', 'date')}</td>{podeNumeroLancamento && <td className="fg-numero-lancamento" title="Duplo clique para ajustar" onDoubleClick={() => abrirAjusteNumero('extrato', item)}>{item.id}</td>}<td>{campoEdicao('descricao_original')}</td><td>{campoEdicao('natureza')}</td>
                       <td>{campoEdicao('valor', 'number')}</td><td>{campoEdicao('saldo', 'number')}</td>
                     </> : <>
-                      <td style={estilosColunas.esquerda}>{textoFixo(item.data_lancamento, 11)}</td>{podeNumeroLancamento && <td className="fg-numero-lancamento" style={estilosColunas.direita} title="Duplo clique para ajustar" onDoubleClick={() => abrirAjusteNumero('extrato', item)}>{item.id}</td>}<td style={estilosColunas.esquerda}>{textoFixo(item.descricao_original, 61)}</td>
+                      <td style={estilosColunas.esquerda}>{textoFixo(item.data_lancamento, 11)}</td>{podeNumeroLancamento && <td className="fg-numero-lancamento" style={estilosColunas.direita} title="Duplo clique para ajustar" onDoubleClick={() => abrirAjusteNumero('extrato', item)}>{item.id}</td>}<td style={estilosColunas.esquerda}>{textoFixo(item.descricao_original, 122)}</td>
                       <td style={estilosColunas.direita}>{textoNumero(item.natureza, 13) + '  '}</td><td style={estilosColunas.direita}>{textoNumero(valorExtrato(item), 14)}</td>
                       <td style={estilosColunas.direita}>{textoNumero(saldoExtrato(item.saldo), 14)}</td>
                     </>}
